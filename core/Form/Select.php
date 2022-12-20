170d70639dd5f01320e1527bb213b9866a395308
6-ON <aminhatim6@gmail.com>
2022-12-20 11:54:23 +0100

    implementing Select Widget

--- core/Form/Select.php
+++ core/Form/Select.php
@@ -11,33 +11,47 @@ class Select
     public Model $model;
 
 
-    public $data;
+    public array $data;
 
 
     public string $attr;
+    public string $dataModel;
+    public array $options;
 
 
-    public function __construct(Model $model, string $attr,string $dataModel)
+    public function __construct(Model $model, string $attr, string $dataModel,string $columnToShow,$FK='')
     {
         $this->model = $model;
         $this->attr = $attr;
-        $data = $dataModel::getAll();
-        echo '<pre>';
-        var_dump($data);
-        echo '</pre>';
-        exit;
+        $this->data = $dataModel::getAll();
+        $this->dataModel =$dataModel;
+        $this->loadOptions($columnToShow,$FK);
 
+    }
+
+
+    public function loadOptions($columnToShow,$FK='')
+    {
+
+        foreach ($this->data as $row) {
+            $rowPK = $row->{$this->dataModel::primaryKey()};
+            $rowLabel = $row->{$columnToShow};
+            $optFormat = (property_exists($this->model,$FK) && $rowPK === $this->model->{$FK})?
+            '<option value="%u" selected >%s</option>':
+            '<option value="%u">%s</option>';
+            
+            $opt =sprintf($optFormat,$rowPK,$rowLabel);
+            $this->options[] = $opt;
+        }
     }
 
     function __toString()
     {
         return sprintf(
             '<select class="form-select col" name="%s">
-        <option>Open this select menu</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
-    </select>',
+            ' . implode(PHP_EOL,$this->options) . '
+            </select>',
+            $this->attr
 
         );
     }
