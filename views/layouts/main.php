c1daac9b4e7cb53408fc20e3b66d46e9ab676277
6-ON <aminhatim6@gmail.com>
2022-12-20 11:57:37 +0100

    fix some bootstrap issues

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -212,7 +212,8 @@ use app\core\Application;
         </p>
     </section>
     <!-- footer section -->
-
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.js"></script>
     <script type="text/javascript" src="js/custom.js"></script>
