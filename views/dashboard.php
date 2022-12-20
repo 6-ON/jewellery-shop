84ca28de175cb590c11ab265155e6a1fabf3de29
6-ON <aminhatim6@gmail.com>
2022-12-20 11:57:09 +0100

    add flash notifications to dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,6 +1,29 @@
 <?php
 
+use app\core\Application;
 use app\widgets\Table\Table; ?>
+
+<?php $msg = Application::$app->session->getFlash('created');
+if($msg): ?>
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>Alert!</strong> <?php echo $msg ?>.
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+<?php endif ?>
+<?php $msg = Application::$app->session->getFlash('deleted');
+if($msg): ?>
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong>Alert!</strong> <?php echo $msg ?>.
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+<?php endif ?>
+<?php $msg = Application::$app->session->getFlash('edited');
+if($msg): ?>
+<div class="alert alert-info alert-dismissible fade show" role="alert">
+    <strong>Alert!</strong> <?php echo $msg ?>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+<?php endif ?>
 
 <div class="heading_container d-flex justify-content-center">
     <h2 class="mb-5">
@@ -9,6 +32,7 @@ use app\widgets\Table\Table; ?>
 </div>
 
 <div class="container-fluid p-md-4" style="overflow-x: scroll;">
+    <a type="button" class="btn text-bg-success mb-2" href="/createProduct">add</a>
 
     <?php $table = Table::begin($columns); ?>
 
