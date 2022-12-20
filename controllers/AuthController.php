1cfb39dd38840531b023a1465bc69691dc42d842
6-ON <aminhatim6@gmail.com>
2022-12-20 11:27:47 +0100

    add create/edit Product

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -4,21 +4,25 @@ namespace app\controllers;
 
 use app\core\Application;
 use app\core\Controller;
+use app\core\exception\ForbiddenException;
 use app\core\exception\NotFoundException;
+use app\core\middlewares\AuthMiddleware;
 use app\middlewares\AdminMiddleware;
 use app\core\Request;
 use app\core\Response;
 use app\models\Category;
 use app\models\LoginForm;
 use app\models\Product;
+use app\models\ProductUpdate;
 use app\models\User;
 
 class AuthController extends Controller
 {
     public function __construct()
     {
-        
-        $this->registerMiddleware(new AdminMiddleware(['dashboard']));
+
+        $this->registerMiddleware(new AdminMiddleware(['dashboard','createProduct','editProduct']));
+        $this->registerMiddleware(new AuthMiddleware([''],['login','register']));
     }
 
     public function logout(Request $request, Response $response)
@@ -93,51 +97,80 @@ class AuthController extends Controller
     public function createProduct(Request $request, Response $response)
     {
         $this->setLayout('main');
-        
+        $product = new Product();
+
         if ($request->isPost()) {
-            $product = new Product();
-            $product->loadData($request->getBody());
-            if ($product->validate() && $product->save()) {
+            $body = $request->getBody();
+            $body['image'] = $_FILES['image']['name'] ?? '';
+            $product->loadData($body);
+            $product->validate();
+            if ($product->validate() && $this->uploadImage() && $product->save()) {
                 Application::$app->session->setFlash('created', 'a Product has been created');
                 $response->redirect("/dashboard");
             }
-            // $response->redirect('/dashsboard');
-            // Product::update(['label' => 'Royal Ringo', 'price' => 124.89], ['id' => 100012]);
-        } else {
-            return $this->render(
-                'product',
-                [
-                    'product' => new Product,
-                ]
-            );
         }
+        return $this->render(
+            'product',
+            [
+                'product' => $product,
+                'cat_ds' => Category::class,
+                'label_column' => 'label',
+                'FK' => 'categoryId'
+
+            ]
+        );
     }
     public function editProduct(Request $request, Response $response)
     {
         $this->setLayout('main');
-        
+        $product = new ProductUpdate();
+
         if ($request->isPost()) {
 
-            $product = new Product();
-            echo '<pre>';
-            var_dump($request->getBody());
-            echo '</pre>';
-            exit;
+            $idProd = Application::$app->session->getFlash('idProd') ?? $response->redirect('/dashboard');
             $product->loadData($request->getBody());
-            
+            $productOld = ProductUpdate::findOne(['id' => $idProd]);
+
+            if (!$productOld) {
+                throw new \Exception("Error Processing Request", 400);
+            }
+
+            if (!empty($_FILES['image']['name'])) {
+                $product->image = $_FILES['image']['name'];
+                $this->uploadImage();
+            }
             if ($product->validate()) {
+                if (Product::update($product->changes($productOld), ['id' => $idProd])) {
+                    Application::$app->session->setFlash('edited', "Product with id $idProd has been Updated");
+                }
                 $response->redirect("/dashboard");
             }
-            // Product::update(['label' => 'Royal Ringo', 'price' => 124.89], ['id' => 100012]);
         } else {
-            $idProd = $request->getBody()['id']?? throw new NotFoundException;
-            $product = Product::findOne(['id' => $idProd]) ?? throw new NotFoundException;
-            return $this->render(
-                'product',
-                [
-                    'product' => $product,
-                ]
-            );
+            $idProd = $request->getBody()['id'] ?? throw new NotFoundException;
+            $product = Product::findOne(['id' => $idProd]);
+            if (!$product) {
+                throw new \Exception("Error Processing Request", 400);
+            }
+            Application::$app->session->setFlash('idProd', $idProd);
         }
+
+        return $this->render(
+            'product',
+            [
+                'product' => $product,
+                'cat_ds' => Category::class,
+                'label_column' => 'label',
+                'FK' => 'categoryId'
+            ]
+        );
+    }
+
+    private function uploadImage()
+    {
+
+        if (!move_uploaded_file($_FILES['image']['tmp_name'], Application::$ROOT_DIR . '/public/uploads/' . $_FILES['image']['name'])) {
+            exit;
+        }
+        return true;
     }
 }
