b51c31de2a8ad6d995a683d9938ead1b675ccd3a
6-ON <aminhatim6@gmail.com>
2022-12-20 11:55:23 +0100

    add actions to prevent relogin after login

--- core/middlewares/AuthMiddleware.php
+++ core/middlewares/AuthMiddleware.php
@@ -8,10 +8,12 @@ use app\core\exception\ForbiddenException;
 class AuthMiddleware extends BaseMiddleware
 {
     public array $actions = [];
+    public array $loggedactions = [];
 
-    public function __construct(array $actions = [])
+    public function __construct(array $actions = [],array $loggedactions =[])
     {
         $this->actions = $actions;
+        $this->loggedactions = $loggedactions;
     }
 
     public function execute()
@@ -20,6 +22,9 @@ class AuthMiddleware extends BaseMiddleware
             if(empty($this->actions) || in_array(Application::$app->controller->action,$this->actions)){
                 throw new ForbiddenException();
             }
-        }
+        }else if(in_array(Application::$app->controller->action,$this->loggedactions)) {
+                Application::$app->response->redirect('/');
+            }
+        
     }
 }
